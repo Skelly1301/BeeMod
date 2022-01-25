df31163f7030ee57417794b543beb4a2b64b25a6
Skelly <88248957+Ful5hii@users.noreply.github.com>
2022-01-25 13:27:12 +0000

    Create 8ball.py

---
+++ commands/fun/8ball.py
@@ -0,0 +1,30 @@
+@bot.command(aliases=['8ball', '8b'])
+async def _8ball(ctx):
+  responses = [
+        'Hell no.',
+        'Prolly not.',
+        'Idk bro.',
+        'Prolly.',
+        'Hell yeah my dude.',
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a Doubt.',
+        'Yes - Definitaly.',
+        'You may rely on it.',
+        'As i see it, Yes.',
+        'Most Likely.',
+        'Outlook Good.',
+        'Yes!',
+        'No!',
+        'Signs a point to Yes!',
+        'Reply Hazy, Try again.',
+        'IDK but u should subscribe to Exotix On Youtube and Follow Him On Insta mahad.ali.khan.',
+        'Better not tell you know.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        "Don't Count on it.",
+        'My reply is No.',
+        'My sources say No.',
+        'Outlook not so good.',
+        'Very Doubtful']
+  await ctx.send(f'{random.choice(responses)}')
