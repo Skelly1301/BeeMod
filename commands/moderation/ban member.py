7ebf9c3183f8139451457eab7e9581d3bb4de60f
Skelly <88248957+Ful5hii@users.noreply.github.com>
2022-01-25 13:28:17 +0000

    Update ban member.py

--- commands/moderation/ban member.py
+++ commands/moderation/ban member.py
@@ -1,5 +1,8 @@
 @bot.command(brief="Bans a server member", description="b!ban <member> [reason]")
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'User {member} has been banned.')
+    try:
+      await member.ban(reason=reason)
+      await ctx.send(f'User {member} has been banned.')
+    except:
+      await ctx.send("The bot has missing permissions\n\nMake sure the Bot's top-most role is above the member's top-most role (the member who you are going to kick)")
