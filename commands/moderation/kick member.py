75ecad5c573394e50bc0fb205c4b054af8a89b8e
Skelly <88248957+Ful5hii@users.noreply.github.com>
2022-01-25 13:28:52 +0000

    Update kick member.py

--- commands/moderation/kick member.py
+++ commands/moderation/kick member.py
@@ -1,4 +1,8 @@
 @bot.command(brief="Kicks a server member", description="b!kick <member> [reason]")
+@commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'User {member} has been kicked.')
+    try:
+      await member.kick(reason=reason)
+      await ctx.send(f'User {member} has been kicked.')
+    except:
+      await ctx.send("The bot has missing permissions\n\nMake sure the Bot's top-most role is above the member's top-most role (the member who you are going to kick)")
