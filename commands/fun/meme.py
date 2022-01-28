942be9c3ec547cff9a3911f2c007b82396393b19
Skelly <88248957+Skelly1301@users.noreply.github.com>
2022-01-28 16:18:13 +0000

    Create meme.py

---
+++ commands/fun/meme.py
@@ -0,0 +1,6 @@
+@bot.command()
+async def meme(ctx):
+    content = get("https://meme-api.herokuapp.com/gimme").text
+    data = json.loads(content,)
+    meme = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
+    await ctx.reply(embed=meme)
