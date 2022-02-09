22661043c4be0a8fec3e87ca09f9963381c2db79
Skelly <88248957+Skelly1301@users.noreply.github.com>
2022-02-09 17:31:02 +0000

    Create bag.py

---
+++ economy/commands/bag.py
@@ -0,0 +1,19 @@
+@bot.command()
+async def bag(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await get_bank_data()
+
+    try:
+        bag = users[str(user.id)]["bag"]
+    except:
+        bag = []
+
+    em = discord.Embed(title = "Bag")
+    for item in bag:
+        name = item["item"]
+        amount = item["amount"]
+
+        em.add_field(name = name, value = amount)
+
+        await ctx.reply(embed=em)
