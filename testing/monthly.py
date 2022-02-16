8b38c8dcdbf279243fc40da0cf894e2f7c1b5847
TheCJayy <97351066+TheCJayy@users.noreply.github.com>
2022-02-16 16:04:03 +0000

    Create monthly.py

---
+++ testing/monthly.py
@@ -0,0 +1,19 @@
+@bot.command()
+@commands.cooldown(1, 2629746, commands.BucketType.user)
+async def monthly(ctx):
+    await open_account(ctx.author)
+
+    user = ctx.author
+
+    users = await get_bank_data()
+
+    earnings = random.randrange(2500)
+
+    begembed = discord.Embed(title=f"{ctx.author.name}", description=f"Someone gave you {earnings} coins! How lucky is that?", color=discord.Color.teal())
+
+    await ctx.reply(embed=begembed)
+
+    users[str(user.id)]["Wallet"] += earnings
+
+    with open("bank.json", 'w') as f:
+        json.dump(users, f)
